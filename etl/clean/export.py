a6d7e4238791d55ae3dc346c39d6fa6db2938418
danlaratta <dan.laratta.jr@gmail.com>
2025-10-16 14:04:34 -0400

    Load update
    moved main.py into a runner package and creating a pipeline context class to control my pipeline runner

---
+++ etl/clean/export.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+RAW_DATA_DIR = ROOT_DIR / 'data' / 'raw'
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# Export week's data
+def export_week_data(df: pd.DataFrame, week: int, base_dir: Path):
+    file_path = base_dir / f'week_{week}_data.csv'
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(file_path, index=False)
+
+
+def bulk_export_week_data(df: pd.DataFrame, start_week: int, end_week: int, base_dir: Path):
+    file_path = base_dir / f'week_{start_week}_to_week{end_week}_data.csv'
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(file_path, index=False)
---
+++ runner/context/pipeline_context.py
@@ -0,0 +1,6 @@
+
+class PipelineContext:
+    def __init__(self) -> None:
+        pass
+
+    
