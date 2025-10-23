d3a16c58234aacadf3c53c59578314baceff888d
danlaratta <dan.laratta.jr@gmail.com>
2025-10-23 12:52:56 -0400

    Analysis Update
    setup of analysis.ipynb file template

--- etl/clean/export.py
+++ etl/clean/export.py
@@ -17,3 +17,9 @@ def bulk_export_week_data(df: pd.DataFrame, start_week: int, end_week: int, base
     file_path = base_dir / f'week_{start_week}_to_week{end_week}_data.csv'
     file_path.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(file_path, index=False)
+
+
+# def bulk_export_week_data(dfs_dict: dict[str, pd.DataFrame], start_week: int, end_week: int, base_dir: Path):
+#     file_path = base_dir / f'week_{start_week}_to_week{end_week}_data.csv'
+#     file_path.parent.mkdir(parents=True, exist_ok=True)
+#     df.to_csv(file_path, index=False)
