46a99a210cc1808c221ca3d7f0b30e1eedb64550
danlaratta <dan.laratta.jr@gmail.com>
2025-10-24 14:09:41 -0400

    Pipeline Fix
    had to clean team id in all clean functions who's tables have team_id

--- etl/clean/clean.py
+++ etl/clean/clean.py
@@ -16,21 +16,25 @@ def clean_teams(df: pd.DataFrame) -> pd.DataFrame:
 
 def clean_games(df: pd.DataFrame) -> pd.DataFrame:
     games_df = df.copy()
+    games_df['team_id'] = games_df['team_id'].replace({33: 31, 34: 32})
     return games_df.sort_values(by=['season_week', 'game_id']).reset_index(drop=True)
 
 
 def clean_players(df: pd.DataFrame) -> pd.DataFrame:
     players_df: pd.DataFrame = df.copy()
+     
     return players_df.sort_values(by='player_name').reset_index(drop=True)
 
 
 def clean_team_game_stats(df: pd.DataFrame) -> pd.DataFrame:
     stats_df = df.copy()
+    stats_df['team_id'] = stats_df['team_id'].replace({33: 31, 34: 32})
     return stats_df.sort_values(by='game_id').reset_index(drop=True)
 
 
 def clean_game_leaders(df: pd.DataFrame) -> pd.DataFrame:
     leaders_df = df.copy()
+    leaders_df['team_id'] = leaders_df['team_id'].replace({33: 31, 34: 32})
     return leaders_df.sort_values(by='game_id').reset_index(drop=True)
 
 
